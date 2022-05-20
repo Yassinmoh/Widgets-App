781ea98471e1b44ef02ab1f63bb1293aefc8ad07
yassin mohamed <yassin_mohamed310@outlook.com>
2022-05-20 17:54:01 +0200

    Add Toggel to dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React ,{useState} from 'react'
 
 const Dropdown = ({options,selected,onSelectedChange}) => {
+    const [open,setOpen]=useState(false)
     const renderedOptions=options.map(option =>{
+        if(option.value === selected.value){
+            return null
+        }
         return (
             <div key={Math.random()} className="item" onClick={()=>onSelectedChange(option)}>
                 {option.label}
@@ -12,10 +16,10 @@ const Dropdown = ({options,selected,onSelectedChange}) => {
         <div className=" ui form ">
             <div className=" field">
                 <label className=" label">Select Color</label>
-                <div className=" ui selection dropdown visible active">
+                <div onClick={()=>setOpen(!open)} className={` ui selection dropdown ${open ? 'visible active' : ''}`}>
                     <i className="dropdown icon"></i>
                     <div className="text">{selected.label}</div>
-                    <div className="menu visible transition">
+                    <div className={`menu ${ open ? 'visible transition' : ''}`}>
                         {renderedOptions}
                     </div>
                 </div>
