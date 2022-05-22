fd82a884a93cd33084497b7bf272c6ba15981381
yassin mohamed <yassin_mohamed310@outlook.com>
2022-05-22 19:45:06 +0200

    add google translate api

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react'
 
-const Dropdown = ({ options, selected, onSelectedChange }) => {
+const Dropdown = ({ label,options, selected, onSelectedChange }) => {
     const [open, setOpen] = useState(false)
     const ref = useRef()
     useEffect(() => {
@@ -32,7 +32,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     return (
         <div ref={ref} className=" ui form ">
             <div className=" field">
-                <label className=" label">Select Color</label>
+                <label className=" label">{label}</label>
                 <div onClick={() => setOpen(!open)} className={` ui selection dropdown ${open ? 'visible active' : ''}`}>
                     <i className="dropdown icon"></i>
                     <div className="text">{selected.label}</div>
--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,15 +1,32 @@
+
 import React,{ useState} from 'react'
 import Dropdown from './Dropdown'
 import options from '../options.json'
+import Convert from '../components/Convert'
+
+
+
 const Translate = () => {
     const [language,setlanguage]=useState(options[0])
+    const [text,setText]=useState('')
+
     return (
         <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Text</label>
+                    <input value={text} onChange={(e) => setText(e.target.value)} />
+                </div>
+            </div>
             <Dropdown 
+                label="Select a Language"
                 options={options} 
                 selected={language}
                 onSelectedChange={setlanguage}
                 />
+                <hr/>
+                <h3 className="ui header">Output</h3>
+                <Convert text={text} language={language}/>
         </div>
     )
 }
